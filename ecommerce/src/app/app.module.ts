f08bf5257ddf96bc41d2f5ffe3e8630ef6e8b7b6
Maria Clara Visconti <claravisconti@hotmail.com>
2020-11-27 01:21:30 -0300

    preparacion

---
+++ ecommerce/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
+import { RegistroComponent } from './registro/registro.component';
+import { LoginComponent } from './login/login.component';
+import { DetalleComponent } from './detalle/detalle.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CatalogoComponent,
+    RegistroComponent,
+    LoginComponent,
+    DetalleComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
